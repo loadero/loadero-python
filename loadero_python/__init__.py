b854b037e58a6bdf3835eaa9793a8fc5ea1e8a8b
rudolfs.zvejs <rudolfs.zvejs@loadero.com>
2022-12-07 14:56:03 +0200

    docs: Clean docs branch

--- loadero_python/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Loadero-Python entry point.
-
-Package contains:
-    - resources sub-package
-    - api_client module
-"""
-
-from . import resources, api_client
--- tests/unit/res/sample_test_script.py
+++
@@ -1,2 +0,0 @@
-def test_on_loadero(driver: TestUIDriver):
-    print("hello test")
