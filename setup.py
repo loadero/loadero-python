b26f0e1913c55b149ebb5e77a79e20b5202c5142
Artūrs Jānis Pētersons <arturs.j.petersons@loadero.com>
2022-12-28 10:48:40 +0200

    1.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (root_path / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="loadero_python",
-    version="1.0.0",
+    version="1.1.0",
     description="Python client for Loadero API",
     long_description=long_description,
     long_description_content_type="text/markdown",
