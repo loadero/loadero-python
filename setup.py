544d4f7b6c24f7b3af19cb1ffef13402ffee8323
Artūrs Jānis Pētersons <arturs.j.petersons@loadero.com>
2024-10-15 12:51:59 +0300

    Update Python version support to >=3.9

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         "Loadero": "https://loadero.com/",
     },
     packages=find_packages(exclude=["tests", "tests.*"]),
-    python_requires=">=3.7, <4",
+    python_requires=">=3.9, <4",
     install_requires=[
         "urllib3==1.26.18",
         "python-dateutil==2.8.2",
